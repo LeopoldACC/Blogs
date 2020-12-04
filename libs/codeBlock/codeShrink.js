ec5add20e00adcbb3be58f4c94feec899e5f94d9
LeopoldACC <1026928673@qq.com>
2020-12-04 21:03:34 +0800

    Site updated: 2020-12-04 21:03:30

--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -1,6 +1,21 @@
 // 代码块收缩
 
 $(function () {
+  var $code_expand = $('<i class="fa fa-chevron-down code-expand" title="折叠代码" aria-hidden="true"></i>');
+
+  $('.code-area').prepend($code_expand);
+  $('.code-expand').on('click', function () {
+    if ($(this).parent().hasClass('code-closed')) {
+      $(this).siblings('pre').find('code').show();
+      $(this).parent().removeClass('code-closed');
+    } else {
+      $(this).siblings('pre').find('code').hide();
+      $(this).parent().addClass('code-closed');
+    }
+  });
+});
+
+/* $(function () {
   var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
 
   $('.code-area').prepend($code_expand);
@@ -14,3 +29,4 @@ $(function () {
     }
   });
 });
+ */
